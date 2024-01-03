67f5822e19e25f22530e519c80dfdeaf613cc91c
nargin <mejumine@gmail.com>
2024-01-03 22:39:17 +0100

    VGA Buffer Macro missing might do it soon

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,18 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
 	loop {}
 }
 
-static GOODBYE: &[u8] = b"Goobye World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	let vga_buffer = 0xb8000 as *mut u8;
+	use core::fmt::Write;
+	// vga_buffer::WRITER.lock().write_str("HAHAHAHHA Dio\nZA WARUDDOOOOO !!!\n\n\n\nChipi chipi chapa chapa\n").unwrap();
+	write!(vga_buffer::WRITER.lock(), "{}", "Texte plutot basique HAHAH").unwrap();
 
-	for (i, &byte) in GOODBYE.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xe;
-		}
-	}
-	
 	loop {}
 }
